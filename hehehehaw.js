0013e884b08f479643c7b90fd43099f57a62b258
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-07-01 11:39:11 -0700

    Update hehehehaw.js

--- hehehehaw.js
+++ hehehehaw.js
@@ -139,48 +139,6 @@ ModAPI.addEventListener("frame", () => {
     }
 });
 
-// Make the menu draggable
-let isDragging = false;
-let currentX;
-let currentY;
-let initialX;
-let initialY;
-let xOffset = 0;
-let yOffset = 0;
-
-function dragStart(e) {
-    initialX = e.clientX - xOffset;
-    initialY = e.clientY - yOffset;
-
-    if (e.target === menuContainer) {
-        isDragging = true;
-    }
-}
-
-function dragEnd(e) {
-    initialX = currentX;
-    initialY = currentY;
-
-    isDragging = false;
-}
-
-function drag(e) {
-    if (isDragging) {
-        e.preventDefault();
-        currentX = e.clientX - initialX;
-        currentY = e.clientY - initialY;
-
-        xOffset = currentX;
-        yOffset = currentY;
-
-        setTranslate(currentX, currentY, menuContainer);
-    }
-}
-
-function setTranslate(xPos, yPos, el) {
-    el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
-}
-
-menuContainer.addEventListener("mousedown", dragStart, false);
-document.addEventListener("mouseup", dragEnd, false);
-document.addEventListener("mousemove", drag, false);
+menuContainer.addEventListener("mousedown", false);
+document.addEventListener("mouseup", false);
+document.addEventListener("mousemove", false);
